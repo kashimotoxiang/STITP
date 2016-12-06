952aea1be8c24571fbf9d7e2a99c6fcea0286c74
weixianbin <weixianbin1996@gmail.com>
2016-12-06 11:06:23 +0800

    画圆

--- popup.js
+++ popup.js
@@ -75,3 +75,20 @@ function saveImage(canvas){
   var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
   window.location.href = image;
 }
+//画圆
+function drawCircle(x,y,canvas){
+  var cxt = canvas.getContext("2d");
+  cxt.strokeStyle = "#FF0000";
+  cxt.beginPath();
+  //context.arc(x,y,r,sAngle,eAngle,counterclockwise);
+  //x	圆的中心的 x 坐标。
+  //y	圆的中心的 y 坐标。
+  //r	圆的半径。
+  //sAngle	起始角，以弧度计。（弧的圆形的三点钟位置是 0 度）。
+  //eAngle	结束角，以弧度计。
+  //counterclockwise	可选。规定应该逆时针还是顺时针绘图。False = 顺时针，true = 逆时针。
+  cxt.arc(x,y,30,0,Math.PI*2,true);
+  cxt.closePath();
+  //cxt.fill();
+  cxt.stroke();
+}
