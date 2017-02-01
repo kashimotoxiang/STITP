9d480bd13365a5a7d6b12577cae2248145202a8b
kashimotoxiang@outlook.com <xiangzaili1995>
2017-02-01 21:35:55 +0800

    v2

--- Chrome_Extends/popup.js
+++ Chrome_Extends/popup.js
@@ -60,3 +60,35 @@ $(document).ready(function () {
     });
 });
 
+function getScroll(){
+  // 获取滚动条到顶部的垂直高度
+  var height = $(document).scrollTop();
+}
+
+function handlePage(){
+  html2canvas(document.body).then(function(canvas){
+    //对canvas 进行处理  在上面画图
+  });
+}
+//保存图片
+function saveImage(canvas){
+  var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+  window.location.href = image;
+}
+//画圆
+function drawCircle(x,y,canvas){
+  var cxt = canvas.getContext("2d");
+  cxt.fillStyle = "rgba(255,0,0,0.3)"; //30%的透明度
+  cxt.beginPath();
+  //context.arc(x,y,r,sAngle,eAngle,counterclockwise);
+  //x	圆的中心的 x 坐标。
+  //y	圆的中心的 y 坐标。
+  //r	圆的半径。
+  //sAngle	起始角，以弧度计。（弧的圆形的三点钟位置是 0 度）。
+  //eAngle	结束角，以弧度计。
+  //counterclockwise	可选。规定应该逆时针还是顺时针绘图。False = 顺时针，true = 逆时针。
+  cxt.arc(x,y,30,0,Math.PI*2,true);
+  cxt.closePath();
+  //cxt.fill();
+  cxt.stroke();
+}
